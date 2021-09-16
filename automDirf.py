41bd47c78a7dba201a9956f073e57f7f0803c413
Lucas Sonnewend <39138298+lrsonnewend@users.noreply.github.com>
2021-09-16 17:41:30 -0300

    add images/update .env/insert try-exception

--- automDirf.py
+++ automDirf.py
@@ -3,6 +3,8 @@ import time #lib para tratar tempos de pausa
 import os #lib para manipular interações com o S.O
 from decouple import config #lib para leitura de arquivo .env
 import csv #lib para manipulação de .csv
+from RPA.Desktop import Desktop #lib para tratar as automatizações desktop
+desktop = Desktop() #criando instancia da lib
 
 #libs para envio de email
 import smtplib as smtp
@@ -16,6 +18,7 @@ from email import encoders
 #install pyautogui
 #install Pillow
 #install python-decouple
+#install rpaframework
 
 #set variaveis de ambiente
 PATH_PDF = config('DIR_PDF')
@@ -25,10 +28,15 @@ SELEC_CNPJ = config('SELEC_CNPJ')
 USER_MAIL = config('LOGIN_MAIL')
 PASS_MAIL = config('PASS_MAIL')
 DIRF_EXEC = config('DIRF_EXEC')
-DIRF_EXEC = "\"'"+DIRF_EXEC+"\"'""
-print(DIRF_EXEC)
-#constantes uteis
-dirf_exec = '"C:/Arquivos de Programas RFB/Dirf2021/pgdDirf.exe"'
+IMPRESSORA_IMG = config('IMPRESSORA_IMG')
+COMP_RENDIMENTO_IMG = config('COMPROVANTE_RENDIMENTO_IMG')
+COMP_BENEFIC_IMG = config('COMPROVANTE_BENEFIC_IMG')
+LBL_CPF_IMG = config('FIELD_CPF_IMG')
+BTN_EXEC_IMG = config('BUTTON_EXEC_IMG')
+HEADER_IMG = config('HEADER_IMG')
+BTN_SAVE_IMG = config('BUTTON_SAVE_IMG')
+BTN_CLOSE_IMG = config('BUTTON_CLOSE_IMG')
+ICON_SAVE_IMG = config('ICON_SAVE_IMG')
 
 
 def PauseStep():
@@ -38,7 +46,11 @@ def PauseStep():
 
 def OpenExec(path):
     #abrindo o programa Dirf
-    os.system(path)
+    try:
+        desktop.open_application(path)
+
+    except (OSError, err):
+        print(f'Erro ao abrir programa: {err}')
 
 
 
@@ -49,7 +61,7 @@ def ClickCNPJ():
     #acessando declaração
     time.sleep(1)
     pag.click(SELEC_CNPJ)
-    time.sleep(3)
+    time.sleep(4)
 
 
 
@@ -64,73 +76,39 @@ def CheckLoadProg(img):
 def EnterOpImpress():
     time.sleep(4)
     #entrando na opção impressão
-    pag.moveTo(163, 60)
-    pag.click()
+    #pag.moveTo(163, 60)
+    pag.click(IMPRESSORA_IMG)
     time.sleep(1)
-    pag.moveTo(202, 122)
+    pag.moveTo(pag.locateOnScreen(COMP_RENDIMENTO_IMG))
     time.sleep(0.5)
-    pag.moveTo(359, 141)
-    pag.click()
-    pag.PAUSE = 0.8
+    pag.click(COMP_BENEFIC_IMG)
+    time.sleep(1)
 
 
 
 def FindBenefic(num_cpf):
     #encontrar beneficiário
-    pag.moveTo(367, 178)
-    pag.click()
+    #pag.moveTo(367, 178)
+    pag.click(LBL_CPF_IMG)
     pag.write(num_cpf)
-    pag.moveTo(870, 561)
-    pag.click()
-    pag.moveTo(465, 255)
-    pag.click()
+    pag.click(BTN_EXEC_IMG)
+    coord_img = pag.locateOnScreen(HEADER_IMG)
+    pag.moveTo(coord_img[0],coord_img[1])
     time.sleep(1)
+    pag.doubleClick(coord_img[0], coord_img[1]+25)
+    time.sleep(4)
     
 
 
-def SendImpress():
-    #enviar para impressão
-    pag.moveTo(786, 601)
-    pag.click()
-    time.sleep(1)
-
-
-
 def SavePDF(num_cpf):
-    pag.PAUSE = 1
+    pag.PAUSE = 2
     #salvando arquivo PDF em diretório (estático)
-    pag.moveTo(169,43)
-    pag.click()
-    pag.moveTo(558, 486)
-    pag.click()
+    pag.click(ICON_SAVE_IMG)
     path_aux = PATH_PDF+'arquivo'+num_cpf+'.pdf'
     pag.write(path_aux)
-    pag.moveTo(812, 544)
-    pag.click()
-    pag.moveTo(1193, 16)
-    pag.click()
+    pag.click(BTN_SAVE_IMG)
+    pag.click(pag.locateCenterOnScreen(BTN_CLOSE_IMG))
     return path_aux
-
-
-
-'''def SendEmail(dest, anexo):
-    #criando integraçao com outlook
-    outlook = win32.Dispatch('outlook.application')
-
-    #criando instancia de email
-    email = outlook.CreateItem(0)
-
-    #configurando informações
-    email.To = dest
-    email.Subject = "Teste envio de relatório"
-    email.HTMLBody = """
-        <p>Olá, segue em anexo o seu comprovante de beneficiário</p>
-        <p>Att,</p>
-        <p>RPA Python</p>
-    """
-    email.Attachments.Add(anexo)
-    email.Send()
-    print('email enviado!')'''
 
 
 
@@ -151,39 +129,43 @@ def SendMailSMTP(dest, anexo):
     #login no servidor
     server.login(login, senha)
 
-    #montando email
-    body = """
-        <p>Olá, segue em anexo o seu comprovante de beneficiário</p>
-        <p>Att,</p>
-        <p>RPA Python</p>
-    """
-    mail = MIMEMultipart()
-    mail['From'] = login
-    mail['To'] = 'lucas.lr184@gmail.com'
-    mail['Subject'] = 'Teste envio de relatório'
-    mail.attach(MIMEText(body,'html'))
+    try:
+        #montando email
+        body = """
+            <p>Olá, segue em anexo o seu comprovante de beneficiário</p>
+            <p>Att,</p>
+            <p>RPA Python</p>
+        """
+        mail = MIMEMultipart()
+        mail['From'] = login
+        mail['To'] = dest
+        mail['Subject'] = 'Teste envio de relatório'
+        mail.attach(MIMEText(body,'html'))
 
-    #abrindo arquivo .pdf
-    attachment = open(anexo, 'rb')
-    
-    #definindo nome do arquivo a ser anexado
-    file_name = anexo[43:]
+        #abrindo arquivo .pdf
+        attachment = open(anexo, 'rb')
+        
+        #definindo nome do arquivo a ser anexado
+        file_name = anexo[43:]
 
-    #lendo arquivo e transformando em base64
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', f'attachment; filename={file_name}')
+        #lendo arquivo e transformando em base64
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload((attachment).read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f'attachment; filename={file_name}')
 
-    #colocando anexo no email
-    mail.attach(part)
+        #colocando anexo no email
+        mail.attach(part)
 
-    #fechando arquivo
-    attachment.close()
-    
-    #enviando email
-    server.sendmail(mail['From'], mail['To'],mail.as_string())
-    server.quit()
+        #fechando arquivo
+        attachment.close()
+        
+        #enviando email
+        server.sendmail(mail['From'], mail['To'],mail.as_string())
+        server.quit()
+
+    except err:
+        print(f'Erro ao enviar email: {err}')    
 
 
 
@@ -201,18 +183,15 @@ def InsertCPFOnApp():
             else:
                 EnterOpImpress()
                 FindBenefic(i[0])
-                SendImpress()
                 path_anexo = SavePDF(i[0])
                 #funcao para enviar email
                 SendMailSMTP(i[1], path_anexo)
 
 
 
-
-
 def main():
     #iniciando RPA
-    OpenExec(dirf_exec)
+    OpenExec(DIRF_EXEC)
     ClickCNPJ()
     InsertCPFOnApp()
 
